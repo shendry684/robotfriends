7d0ff5929c7e9d189dedb591642a6d28de7f7030
schendry684 <sean.c.hendry@gmail.com>
2020-10-01 15:45:54 -0400

    Clean Repo

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Oderus Urungus',
+    username: 'Bret',
+    attack: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Pustulus Maximus',
+    username: 'Antonette',
+    attack: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Blothar',
+    username: 'Samantha',
+    attack: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Balsac',
+    username: 'Karianne',
+    attack: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Sleazy P',
+    username: 'Kamren',
+    attack: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    attack: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    attack: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    attack: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    attack: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    attack: 'Rey.Padberg@karina.biz'
+  }
+];
